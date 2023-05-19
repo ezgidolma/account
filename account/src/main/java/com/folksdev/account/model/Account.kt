238e97440f6b37b7fbeeaa79bb915b4e5fa153b3
ezgidolma <ezgidolma23@gmail.com>
2023-05-20 00:17:00 +0300

    Initial commit

---
+++ account/src/main/java/com/folksdev/account/model/Account.kt
@@ -0,0 +1,36 @@
+package com.folksdev.account.model
+
+import org.hibernate.annotations.Cascade
+import org.hibernate.annotations.Fetch
+import org.hibernate.annotations.GenericGenerator
+import org.hibernate.annotations.ManyToAny
+import java.math.BigDecimal
+import java.time.LocalDate
+import java.time.LocalDateTime
+import java.util.Date
+import javax.persistence.CascadeType
+import javax.persistence.Entity
+import javax.persistence.FetchType
+import javax.persistence.GeneratedValue
+import javax.persistence.Id
+import javax.persistence.JoinColumn
+import javax.persistence.ManyToOne
+import javax.persistence.OneToMany
+
+
+@Entity
+data class Account(
+        @Id
+        @GeneratedValue(generator = "UUID")
+        @GenericGenerator(name = "UUID", strategy = "org.hibernate.id.UUIDGenerator")
+        val id: String?,
+        val balance:BigDecimal?=BigDecimal.ZERO,
+        val creationDate: LocalDateTime,
+
+        @ManyToOne(fetch = FetchType.LAZY , cascade = [CascadeType.ALL])
+        @JoinColumn(name = "customer_id", nullable = false)
+        val customer : Customer?,
+
+        @OneToMany(mappedBy = "account" , fetch = FetchType.LAZY)
+        val transaction: Set<Transaction>,
+)
